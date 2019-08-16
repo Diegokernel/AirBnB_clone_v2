f9753689949283dc18fe0d0ff09cecfdf6ab7145
muxanz <muxanzz@gmail.com>
2019-08-16 02:48:08 +0000

    lines are adjusted

--- models/review.py
+++ models/review.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This is the review class"""
+"""This is the review class
+"""
 from models.base_model import BaseModel
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
@@ -11,6 +12,7 @@ class Review(BaseModel):
         user_id: user id
         text: review description
     """
+
     __tablename__ = 'reviews'
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
